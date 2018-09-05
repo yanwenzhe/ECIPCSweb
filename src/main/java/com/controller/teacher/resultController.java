50a543f3ec22459c9f60649b9adbc89bfff1bc7a
yanwenzhe <2517866043@qq.com>
2018-09-05 10:16:30 +0800

    试验合并

--- src/main/java/com/controller/teacher/resultController.java
+++ src/main/java/com/controller/teacher/resultController.java
@@ -3,7 +3,7 @@ package com.controller.teacher;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import static com.utils.PageName.RESULT;
+import static com.utils.PageNameUtil.RESULT;
 
 @Controller
 @RequestMapping("/teacher")
