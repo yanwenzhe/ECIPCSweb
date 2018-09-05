da0c8d37fd2d0e79d6c2feded522f069c1e93dde
yanwenzhe <2517866043@qq.com>
2018-09-05 11:29:21 +0800

    侧边栏与登录

--- src/main/java/com/utils/PageNameUtil.java
+++ src/main/java/com/utils/PageNameUtil.java
@@ -1,7 +1,7 @@
 package com.utils;
 
 public class PageNameUtil {
-    public final static String LOGIN = "login";
+    public final static String LOGIN = "/system/login/signInUI";
     public final static String COURSEVIEW = "/system/teacher/courseView";
     public final static String RESULT = "/system/teacher/result";
 }
