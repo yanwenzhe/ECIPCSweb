b0e465291833af5e3fcd5d4b66cc058a0acdf47f
Bennyxie <xbh18811379626@163.com>
2018-09-07 15:11:53 +0800

    返回

--- src/main/java/com/controller/professor/AllRequirementController.java
+++ src/main/java/com/controller/professor/AllRequirementController.java
@@ -2,13 +2,17 @@ package com.controller.professor;
 
 import com.dao.IndexRequirementDao;
 import com.entity.IndexRequirement;
+import com.utils.AjaxMessge;
+import com.utils.MsgType;
 import com.utils.PageNameUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -25,12 +29,34 @@ public class AllRequirementController {
         return PageNameUtil.ALL_REQUIREMENT;
     }
 
+    /**
+     * get请求获取所有的指标要求
+     * @return
+     */
     @RequestMapping(value = "getList",method = RequestMethod.GET)
     @ResponseBody
     public Object getAllRequirements()
     {
 
       List<IndexRequirement> list = indexRequirementDao.getAllRequirements();
-      return list;
+      HashMap<String ,Object> hashMap=new HashMap<>();
+      hashMap.put("list",list);
+      return new AjaxMessge().Set(MsgType.Success,hashMap);
+    }
+
+
+    /**
+     * post修改指标要求的描述
+     * @param id
+     * @param description
+     * @return
+     */
+    @RequestMapping(value = "updateList", method = RequestMethod.POST)
+    @ResponseBody
+    public void updateRequirement(@RequestParam("id") String id,
+            @RequestParam("description") String description)
+    {
+        IndexRequirement indexRequirement = new IndexRequirement(id,"",description);
+        indexRequirementDao.updateIndexRequirement(indexRequirement);
     }
 }
